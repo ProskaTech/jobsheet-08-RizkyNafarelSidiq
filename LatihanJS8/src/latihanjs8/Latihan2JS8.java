f3ef4a91b973728c836b138ffa8a9c394360374b
RizkyNafarelSidiq <112645159+RizkyNafarelSidiq@users.noreply.github.com>
2022-10-11 16:49:27 +0700

    Add files via upload
    Nama : Rizky Nafarel Sidiq
    Nim: 21343071
    Pratikum Berorientasi Objek

---
+++ LatihanJS8/src/latihanjs8/Latihan2JS8.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanjs8;
+
+/**
+ *
+ * // Created Rizky Nafarel Sidiq 21343071
+ */
+public class Latihan2JS8 {
+    public static void main(String [] args){
+        int[] ages = new int[100];
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print(ages[i]);
+        }
+    }
+    
+}
---
+++ LatihanJS8/src/latihanjs8/Latihan3JS8.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanjs8;
+
+/**
+ *
+ * // Created Rizky Nafarel Sidiq 21343071
+ */
+public class Latihan3JS8 {
+    public static void main(String[] args){
+        
+        //String Array 4 baris x 2 kolom
+        String[][] dogs = {{"Terry", "brown"}, //baris ke 0
+                           {"Kristin", "white"}, //baris ke 1
+                           {"Toby", "gray"}, //baris ke 2
+                           {"Fido", "black"} //baris ke 3
+                          };
+        System.out.println(dogs[0][0]);
+        // mengakses variabel dogs dengan indeks baris 0, indeks kolom 0
+    }    
+}
